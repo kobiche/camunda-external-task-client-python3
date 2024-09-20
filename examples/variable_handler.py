1de8b0fe1cd87b15d0db42dc530ff40f9927b303
Marcos Caballero <marcosca@cib.de>
2024-09-20 10:04:37 +0200

    Added variable manipulation

--- examples/variable_handler.py
+++ examples/variable_handler.py
@@ -1,3 +1,5 @@
+import sys, os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')))
 from camunda.external_task.external_task import ExternalTask, TaskResult
 from camunda.external_task.external_task_worker import ExternalTaskWorker
 # from src.main import config
@@ -12,14 +14,28 @@ def handle_task(task: ExternalTask) -> TaskResult:
     print(f"Received string: {first_var}")
     print(f"Received integer: {nr_var}")
 
-    result = TaskResult(
-        success=True,
-        task=task,
-        global_variables={},
-        local_variables={}
-    )
+    # result = TaskResult(
+    #     success=True,
+    #     task=task,
+    #     global_variables={},
+    #     local_variables={}
+    # )
 
-    return result
+    # return result
+
+    manipulated_string = first_var + first_var if first_var else None
+    manipulated_integer = nr_var * 2 if nr_var else None
+
+    print(f"Manipulated string: {manipulated_string}")
+    print(f"Manipulated integer: {manipulated_integer}")
+
+    variables = {
+        "manipulatedString": manipulated_string,
+        "manipulatedInteger": manipulated_integer
+    }
+
+    return task.complete(variables)
+    
 
 if __name__ == "__main__":
     worker = ExternalTaskWorker(worker_id="my-worker", base_url="http://localhost:8088/engine-rest")
